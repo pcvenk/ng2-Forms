145e358acaecbea339ae8957374e6f1564b56b84
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-05-25 15:19:29 +0200

    Prepoulating the form with the setValue() of the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,15 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signUpForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      defaultValue: '',
+      gender: 'male'
+    });
   }
 
   // onSubmit(form: NgForm) {
